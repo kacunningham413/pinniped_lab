55bc6dffa83c55cb551cdaeeb4d1ac91d7ccdac8
kacunningham413 <kacunningham413@gmail.com>
2021-02-27 13:41:32 -0800

    [Refactor] Factors out MaskingAnalyzer into its own module.

--- masking_analysis/sound_test.py
+++ masking_analysis/sound_test.py
@@ -1,7 +1,7 @@
 import os
 from masking_analysis.protos import sound_generation_pb2, \
   masking_config_pb2
-from masking_analysis.sound import Sound, MaskingAnalyzer, FreqBand
+from masking_analysis.sound import Sound, FreqBand
 import numpy as np
 import unittest
 
@@ -122,38 +122,6 @@ class SoundTest(unittest.TestCase):
       np.mean(list(spls[FreqBand.from_limits(100, 200)].values())), -100)
     self.assertLess(
       np.mean(list(spls[FreqBand.from_limits(3000, 4000)].values())), -100)
-
-  def test_masking_analyzer_signal_excess(self):
-    signal_gen_config = sound_generation_pb2.SoundGenConfig()
-    signal_gen_config.fs = 5000
-    signal_gen_config.duration = 2
-    signal_gen_config.pure_tone_config.center_freq = 500
-    signal = Sound.sound_from_gen_config(signal_gen_config)
-
-    noise_gen_config = sound_generation_pb2.SoundGenConfig()
-    noise_gen_config.fs = 5000
-    noise_gen_config.duration = 2
-    noise_gen_config.flat_spectrum_noise_config.start_freq = 50
-    noise_gen_config.flat_spectrum_noise_config.stop_freq = 1000
-    noise_gen_config.flat_spectrum_noise_config.filter_order = 10
-    noise = Sound.sound_from_gen_config(noise_gen_config)
-
-    masking_config = masking_config_pb2.MaskingConfig()
-    band_config = masking_config.auditory_band_config
-    for start, stop in [(100, 200), (200, 400), (400, 800)]:
-      band = band_config.auditory_bands.add()
-      band.start_freq = start
-      band.stop_freq = stop
-    band_config.filter_order = 10
-    masking_config.window_duration_ms = 250
-    masking_config.window_step_ms = 250
-
-    analyzer = MaskingAnalyzer(signal, noise, masking_config)
-    se = analyzer.get_signal_excess()
-    self.assertTrue(np.all((se[FreqBand.from_limits(400, 800)] - se[
-      FreqBand.from_limits(100, 200)]) > 10))
-    self.assertTrue(np.all((se[FreqBand.from_limits(400, 800)] - se[
-      FreqBand.from_limits(200, 400)]) > 10))
 
   def test_compute_spectrogram(self):
     # TODO(kane): Test contents of spectrogram
